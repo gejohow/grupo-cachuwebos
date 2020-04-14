a5d8930f95e17468b4879a946cfc2aa971d86e22
Matías Echeverría <mgecheverria@uc.cl>
2020-04-14 18:51:19 -0400

    Users avance

--- xchange/src/routes/users.js
+++ xchange/src/routes/users.js
@@ -16,11 +16,42 @@ router.get('users.list', '/', async (ctx) => {
   });
 });
 
-router.get('users.view', '/:id', loadUser, async (ctx) => {
+router.get('users.view', '/:id/view', loadUser, async (ctx) => {
 	const { user } = ctx.state;
 	await ctx.render('users/view', {
     user,
   });
+});
+
+router.get('users.new', '/new', async (ctx) => {
+	const user = await ctx.orm.user.build();
+  await ctx.render('users/new', {
+    user,
+    submitUserPath: ctx.router.url('users.create'),
+  });
+});
+
+router.post('users.create', '/', async (ctx) => {
+  const user = ctx.orm.user.build(ctx.request.body);
+  // escribir funcion hacer un const con lat y long... hacer el mimso save de favoritos
+  try {
+    await user.save({
+      fields: [
+      	'username',
+        'firstname',
+        'lastname',
+        'password',
+      ],
+    });
+    ctx.redirect(ctx.router.url('session.new'));
+  } catch (validationError) {
+    console.log(validationError);
+    await ctx.render('users/new', {
+      user,
+      error: validationError.errors,
+      submitUserPath: ctx.router.url('users.create'),
+    });
+  }
 });
 
 module.exports = router;
