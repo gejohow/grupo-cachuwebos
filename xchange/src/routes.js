5c1e018e4a7c213e8299a081cb6562034ed3d422
Tomas Cantergiani <tpcantergiani@uc.cl>
2020-04-14 17:29:46 -0400

    comienzo publicacion

--- xchange/src/routes.js
+++ xchange/src/routes.js
@@ -3,6 +3,7 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 
+
 const router = new KoaRouter();
 
 router.use('/', index.routes());
