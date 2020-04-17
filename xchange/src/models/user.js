a7ca443fb6071e426889a39dfa11b7f547f5ac86
Matías Echeverría <mgecheverria@uc.cl>
2020-04-17 12:54:30 -0400

    CRUD Users

--- xchange/src/models/user.js
+++ xchange/src/models/user.js
@@ -1,3 +1,14 @@
+const bcrypt = require('bcrypt');
+
+const PASSWORD_SALT = 10;
+
+async function buildPasswordHash(instance) {
+  if (instance.changed('password')) {
+    const hash = await bcrypt.hash(instance.password, PASSWORD_SALT);
+    instance.set('password', hash);
+  }
+}
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     username: {
@@ -8,6 +19,14 @@ module.exports = (sequelize, DataTypes) => {
     lastName: DataTypes.STRING,
     password: DataTypes.STRING,
   }, {});
+
+
+  user.beforeUpdate(buildPasswordHash);
+  user.beforeCreate(buildPasswordHash);
+
+  user.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.password);
+  };
 
   user.associate = function associate() {
     // associations can be defined here. This method receives a models parameter.
--- xchange/src/routes/users.js
+++ xchange/src/routes/users.js
@@ -13,15 +13,23 @@ router.get('users.list', '/', async (ctx) => {
 	const usersList = await ctx.orm.user.findAll();
   await ctx.render('users/index', {
     usersList,
+    newUserPath: ctx.router.url('users.new'),
+    editUserPath: (user) => ctx.router.url('users.edit', { id: user.id }),
+    deleteUserPath: (user) => ctx.router.url('users.delete', { id: user.id }),
+    viewUserPath: (user) => ctx.router.url('users.view', { id: user.id }),
   });
 });
+
 
 router.get('users.view', '/:id/view', loadUser, async (ctx) => {
 	const { user } = ctx.state;
 	await ctx.render('users/view', {
     user,
+    editUserPath: (editedUser) => ctx.router.url('users.edit', { id: editedUser.id }),
+    deleteUserPath: (deletedUser) => ctx.router.url('users.delete', { id: deletedUser.id }),
   });
 });
+
 
 router.get('users.new', '/new', async (ctx) => {
 	const user = await ctx.orm.user.build();
@@ -33,17 +41,16 @@ router.get('users.new', '/new', async (ctx) => {
 
 router.post('users.create', '/', async (ctx) => {
   const user = ctx.orm.user.build(ctx.request.body);
-  // escribir funcion hacer un const con lat y long... hacer el mimso save de favoritos
   try {
     await user.save({
       fields: [
       	'username',
-        'firstname',
-        'lastname',
+        'firstName',
+        'lastName',
         'password',
       ],
     });
-    ctx.redirect(ctx.router.url('session.new'));
+    ctx.redirect(ctx.router.url('users.list'));
   } catch (validationError) {
     console.log(validationError);
     await ctx.render('users/new', {
@@ -53,5 +60,39 @@ router.post('users.create', '/', async (ctx) => {
     });
   }
 });
+
+router.get('users.edit', '/:id/edit', loadUser, async (ctx) => {
+  const { user } = ctx.state;
+  await ctx.render('users/edit', {
+    user,
+    submitUserPath: ctx.router.url('users.update', { id: user.id }),
+  });
+});
+
+router.patch('users.update', '/:id', loadUser, async (ctx) => {
+  const { user } = ctx.state;
+  try {
+    const {
+      username, firstName, lastName, password,
+    } = ctx.request.body;
+    await user.update({
+      username, firstName, lastName, password,
+    });
+    ctx.redirect(ctx.router.url('users.list'));
+  } catch (validationError) {
+    await ctx.render('users/edit', {
+      user,
+      errors: validationError.errors,
+      submitUserPath: ctx.router.url('users.update', { id: user.id }),
+    });
+  }
+});
+
+router.del('users.delete', '/:id', loadUser, async (ctx) => {
+  const { user } = ctx.state;
+  await user.destroy();
+  ctx.redirect(ctx.router.url('users.list'));
+});
+
 
 module.exports = router;
