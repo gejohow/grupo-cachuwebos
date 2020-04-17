92b233a65f783b9905c57a23c8e9ce999a07297b
YOUR NAME <gajohow@uc.cl>
2020-04-17 17:11:40 -0400

    negotiations

--- xchange/src/routes/negotiations.js
+++ xchange/src/routes/negotiations.js
@@ -2,6 +2,11 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+async function loadNegotiation(ctx, next) {
+  ctx.state.negotiation = await ctx.orm.negotiation.findByPk(ctx.params.id);
+  return next();
+}
+
 router.get('negotiations.list', '/', async (ctx) => {
   const negotiationsList = await ctx.orm.negotiation.findAll();
   await ctx.render('negotiations/index', {
@@ -9,5 +14,59 @@ router.get('negotiations.list', '/', async (ctx) => {
   });
 });
 
+router.get('negotiations.new', '/new', async (ctx) => {
+  const negotiation = ctx.orm.course.build();
+  await ctx.render('negotiations/new', {
+    negotiation,
+    submitCoursePath: ctx.router.url('negotiations.create'),
+  });
+});
+
+router.post('negotiations.create', '/', async (ctx) => {
+  const negotiation = ctx.orm.negotiation.build(ctx.request.body);
+  try {
+    await negotiation.save({ fields: ['user1', 'user2', 'objects1', 'objects2'] });
+    ctx.redirect(ctx.router.url('negotiations.list'));
+  } catch (validationError) {
+    await ctx.render('negotiations.new', {
+      negotiation,
+      errors: validationError.errors,
+      submitCoursePath: ctx.router.url('negotiations.create'),
+    });
+  }
+});
+
+router.get('negotiations.edit', '/:id/edit', loadNegotiation, async (ctx) => {
+  const { negotiation } = ctx.state;
+  await ctx.render('negotiations/edit', {
+    negotiation,
+    submitCoursePath: ctx.router.url('negotiations.update', { id: negotiation.id }),
+  });
+});
+
+router.patch('negotiations.update', '/:id', loadNegotiation, async (ctx) => {
+  const { negotiation } = ctx.state;
+  try {
+    const {
+      user1, user2, objects1, objects2,
+    } = ctx.request.body;
+    await negotiation.update({
+      user1, user2, objects1, objects2,
+    });
+    ctx.redirect(ctx.router.url('negotiations.list'));
+  } catch (validationError) {
+    await ctx.render('negotiations/edit', {
+      negotiation,
+      errors: validationError.errors,
+      submitCoursePath: ctx.router.url('negotiations.update', { id: negotiation.id }),
+    });
+  }
+});
+
+router.del('negotiations.delete', '/:id', loadNegotiation, async (ctx) => {
+  const { negotiation } = ctx.state;
+  await negotiation.destroy();
+  ctx.redirect(ctx.router.url('negotiations.list'));
+});
 
 module.exports = router;
