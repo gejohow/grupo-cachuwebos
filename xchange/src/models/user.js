850ce382b4705e2fe74f8fd05ad562bbff938f58
Matías Echeverría <mgecheverria@uc.cl>
2020-04-18 19:07:34 -0400

    Users & reviews error union

--- xchange/src/models/user.js
+++ xchange/src/models/user.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   user.associate = function associate() {
-    // associations can be defined here. This method receives a models parameter.
+    user.hasMany(models.review);
   };
 
   return user;
